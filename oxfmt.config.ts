a75cdda04779611da0c056684a84af6d63aa4c60
Jack Baron <jackmbaron@gmail.com>
2026-06-03 23:30:59 +0100

    this breaks

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -1,1 +1,1 @@
-export { default } from "#/oxfmt.ts";
+export { default } from "./src/oxfmt.ts";
--- oxlint.config.ts
+++ oxlint.config.ts
@@ -1,6 +1,6 @@
 import type { OxlintConfig } from "oxlint";
 import { defineConfig } from "oxlint";
-import { default as common } from "#/oxlint/common.ts";
+import { default as common } from "./src/oxlint/common.ts";
 
 const config: OxlintConfig = defineConfig({
   extends: [common],
